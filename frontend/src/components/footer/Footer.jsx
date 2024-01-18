ea25365eff515370491b566e022fb38c296dc704
chanita23 <sanamapeca@gmail.com>
2024-01-18 12:02:27 +0100

    componentes Header y Footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,1 +1,16 @@
-import './Footer.css';
+import "./Footer.css";
+
+
+export const Footer = () => {
+  return (
+    
+      <footer>
+        <div className="contenedor">
+          <h3>Hooking.com</h3>
+        </div>
+      </footer>
+    
+  );
+};
+
+export default Footer;
--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -2,8 +2,17 @@ import "./Header.css";
 
 export const Header = () => {
   return (
-    <header>
-      <h1>Hooking</h1>
-    </header>
+    
+      <header>
+        <nav className="nav">
+          <a href="#" className="site title">Hooking</a>
+          <button className="contained">Mis Reservas</button>
+        </nav>
+      </header>
+    
   );
 };
+    
+          
+        
+export default Header;
