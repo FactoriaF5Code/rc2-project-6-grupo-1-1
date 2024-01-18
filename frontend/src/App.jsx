5bac1f0c43d237836687aeb5c4d223e7592a2a76
Johnny <jonathanbc2012@gmail.com>
2024-01-18 12:05:00 +0100

    form css and jsx created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import "./App.css"
-import { Header } from "./components/header/Header";
-
-
+import "./App.css";
+import { Form } from "./components/form/Form";
 
 function App() {
   return (
     <>
-      <Header />      
+      <Form />
     </>
   );
 }
--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -1,1 +1,30 @@
-import './Form.css';
+import "./Form.css";
+
+export const Form = () => {
+  return (
+    <section className="chooseDateContainer">
+      <h2>Hotel Número 1</h2>
+      <div className="stars">
+        <img src="../../../public/Star.svg" alt="star icon" />
+        <img src="../../../public/Star.svg" alt="star icon" />
+        <img src="../../../public/Star.svg" alt="star icon" />
+        <img src="../../../public/Star.svg" alt="star icon" />
+      </div>
+      <div className="formContainer">
+      <h3>Completa tu reserva</h3>      
+      <form>
+        <div>
+          <label htmlFor="">Fecha de entrada</label>
+          <input type="date" />
+        </div>
+        <div>
+          <label htmlFor="">Fecha de Salida</label>
+          <input type="date" />
+        </div>
+
+        <input type="submit" value={"¡Reserva Ahora!"} />
+      </form>
+      </div>
+    </section>
+  );
+};
