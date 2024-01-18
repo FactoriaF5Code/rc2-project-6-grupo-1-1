a3b6bfd9114da1142f463671e8fe77780b14a24e
Iciar Rodríguez <iciar@MacBook-Pro-de-Iciar.local>
2024-01-18 14:24:45 +0100

    Añadida paginación de los hoteles, además de las flechas que navegan por la paginación

--- frontend/src/components/articles/DetailsHotel.jsx
+++ frontend/src/components/articles/DetailsHotel.jsx
@@ -3,8 +3,7 @@ import { useEffect, useState } from "react";
 
 import Star from "../../../public/star.svg";
 
-const Hotel = ({hotel}) => {
-
+const Hotel = ({ hotel }) => {
   const randomStars = Math.floor(Math.random() * 6);
 
   return (
@@ -14,8 +13,8 @@ const Hotel = ({hotel}) => {
         <div className="nombreHotel">
           <h1>{hotel.name}</h1>
           <div>
-          {[...Array(randomStars)].map((_, index) => (
-            <img key={index} src={Star} className="star" alt="Estrella" />
+            {[...Array(randomStars)].map((_, index) => (
+              <img key={index} src={Star} className="star" alt="Estrella" />
             ))}
           </div>
         </div>
@@ -29,11 +28,13 @@ const Hotel = ({hotel}) => {
         <button>¡Reserva ahora!</button>
       </div>
     </article>
-  )
-}
+  );
+};
 
 export const DetailsHotel = () => {
   const [hotels, setHotels] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const hotelsPerPage = 2;
 
   useEffect(() => {
     fetch("http://localhost:8080/api/hotels")
@@ -42,11 +43,43 @@ export const DetailsHotel = () => {
       .catch((error) => console.error("Error fetching hotels:", error));
   }, []);
 
+  const totalPages = Math.ceil(hotels.length / hotelsPerPage);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const startIndex = (currentPage - 1) * hotelsPerPage;
+  const endIndex = startIndex + hotelsPerPage;
+  const currentHotels = hotels.slice(startIndex, endIndex);
+
   return (
     <div>
-      {hotels.map((hotel) => (
+      {currentHotels.map((hotel) => (
         <Hotel key={hotel.id} hotel={hotel} />
       ))}
+
+      <div className="pagination">
+        <button onClick={handlePrevPage}>&lt;</button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button key={index + 1} onClick={() => handlePageChange(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+        <button onClick={handleNextPage}>&gt;</button>
+      </div>
     </div>
   );
 };
