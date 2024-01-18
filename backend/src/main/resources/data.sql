558350ae80e3573eb99869ff5f89d24047843217
Johnny <jonathanbc2012@gmail.com>
2024-01-18 13:59:41 +0100

    data.sql changed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,12 @@
-insert into hotels(name, description, photo_url, price_per_night) values ('Hotel Miami', 'Fabuloso', 'placeholder', 300.0 ); 
-insert into hotels(name, description, photo_url, price_per_night) values ('hotel la roca', 'un hotel', 'placeholder', 300.0 ); 
-insert into hotels(name, description, photo_url, price_per_night) values ('hotel pernambuco', 'un hotel', 'placeholder', 300.0 ); 
-insert into hotels(name, description, photo_url, price_per_night) values ('hotel tibidabo', 'un hotel', 'placeholder', 300.0 ); 
+INSERT INTO hotels(name, description, photo_path, price_per_night)
+VALUES
+('Hotel Azul', 'Un lugar tranquilo con vista al mar', 'assets/img/foto1.jpg', 120.50),
+('Hotel Estrella', 'Un refugio elegante en el centro de la ciudad', 'assets/img/foto2.jpg', 89.99),
+('Hotel del Bosque', 'Experimenta la naturaleza en nuestro acogedor hotel', 'assets/img/foto3.jpg', 150.25),
+('Hotel de Montaña', 'Disfruta de la serenidad en las alturas', 'assets/img/foto4.jpg', 110.00),
+('Hotel Elegante', 'Lujo y comodidad en cada rincón', 'assets/img/foto5.jpg', 200.75),
+('Hotel Sereno', 'Un lugar de paz y relajación', 'assets/img/foto6.jpg', 95.50),
+('Hotel Dorado', 'Experimenta la opulencia en nuestro hotel de cinco estrellas', 'assets/img/foto7.jpg', 180.00),
+('Hotel Cielo', 'Vistas impresionantes desde las alturas', 'assets/img/foto8.jpg', 135.25),
+('Hotel Encanto', 'Descubre la magia en nuestro hotel boutique', 'assets/img/foto9.jpg', 120.90),
+('Hotel del Sol', 'Disfruta del sol y la playa en nuestro resort', 'assets/img/foto10.jpg', 170.00);
